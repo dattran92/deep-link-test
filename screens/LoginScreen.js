21c4352ad0d8fbb0e57ec5b8709f8d7bcb5d1810
Dat Tran <tranthanhdat.it@gmail.com>
2022-07-19 16:37:36 +0700

    App login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Linking from 'expo-linking';
 
 export default function LoginScreen() {
+  const doLogin = () =>{
+    Linking.openURL('https://test-deep-link.dattran.one/app-login.html?username=dat');
+  }
+
   return (
     <View style={styles.container}>
       <Text>Login</Text>
+      <Button
+        title='Authenticate for webapp'
+        onPress={doLogin}
+      />
     </View>
   );
 }
