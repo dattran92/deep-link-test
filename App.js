a953785ba29a23acbb2ddd5f9af1ec6c0690fc6f
Dat Tran <tranthanhdat.it@gmail.com>
2022-07-19 15:29:24 +0700

    handle deeplink

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as Linking from 'expo-linking';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const prefix = Linking.createURL('/')
+
+const Stack = createStackNavigator()
 
 export default function App() {
+  const [data, setData] = useState(null);
+
+  const linking = {
+    prefixes: [prefix],
+    config: {
+      screens: {
+        Home: 'home',
+        Login: 'login'
+      }
+    }
+  }
+
+  const handleDeepLink = (e) => {
+    const _data = Linking.parse(e.url);
+    setData(_data)
+  }
+
+  useEffect(() => {
+    Linking.addEventListener('url', handleDeepLink)
+
+    return () => {
+      Linking.removeEventListener('url')
+    }
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
